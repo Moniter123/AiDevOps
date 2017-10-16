905552b704da44158287659a65020d9a29e9bfe9
余先生 <YoLoveLIfe@outlook.com>
2017-10-16 22:11:29 +0800

    Revert "DB complete"

--- apps/execute/service/catch/basic.py
+++ apps/execute/service/catch/basic.py
@@ -8,23 +8,22 @@ class BasicAnsibleService(AnsibleService):
     def __init__(self,hostlist):
         self.maker = Maker()
         self.maker.inventory_maker(hostlist)
-        self.hostlist = hostlist
         super(BasicAnsibleService,self).__init__(self.maker.filename)
 
-    def run(self,tasklist):
+    def run(self,hostlist,tasklist):
         callback = ResultCallback()
         self.push_callback(callback)
         super(BasicAnsibleService,self).run(tasklist,self.maker)
         if callback.status == 1 :
             list = callback.ResultExtract()
-            self.update(list)
+            self.update(list,hostlist)
         else:
-            for host in self.hostlist:
+            for host in hostlist:
                 host.status = callback.status
                 host.save()
 
-    def update(self,list):
-        for host in self.hostlist:
+    def update(self,list,hostlist):
+        for host in hostlist:
             host.coreness = list[0]
             host.memory = list[1]
             host.root_disk = list[2]
